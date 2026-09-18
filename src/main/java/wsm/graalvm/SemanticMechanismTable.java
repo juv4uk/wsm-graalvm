97a510929b8f234483f24c5a3b5d6efa8b741b75
juv4uk <85576956+juv4uk@users.noreply.github.com>
2026-09-18 17:22:19 +0300

    cleanup: retire Java mechanism 1022 in favor of core Lisp

--- src/main/java/wsm/graalvm/SemanticMechanismTable.java
+++ src/main/java/wsm/graalvm/SemanticMechanismTable.java
@@ -22,7 +22,6 @@ public final class SemanticMechanismTable {
             "0005", SemanticMechanismTable::invoke0005,
             "0006", SemanticMechanismTable::invoke0006,
             "1016", SemanticMechanismTable::invoke1016,
-            "1022", SemanticMechanismTable::invoke1022,
             "1052", SemanticMechanismTable::invoke1052,
             "1061", SemanticMechanismTable::invoke1061
     );
@@ -99,10 +98,4 @@ public final class SemanticMechanismTable {
         return new Value.StringValue(CanonicalSerializer.write(args[0]));
     }
 
-    private static Object invoke1022(Object[] args) {
-        WsmError.arity(args, 2, "1022");
-        return WsmNode.Structural.equals(args[0], args[1])
-                ? Value.record("structural-relation", "same")
-                : Value.record("structural-relation", "distinct");
-    }
 }
