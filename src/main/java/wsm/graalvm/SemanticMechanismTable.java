1f48c1e333a3a5aeae528d20141a21eeb04432e4
juv4uk <85576956+juv4uk@users.noreply.github.com>
2026-09-18 17:05:07 +0300

    table: drop my stray 1017 mapping (build was broken 1-minute on main; rebuilt GREEN)

--- src/main/java/wsm/graalvm/SemanticMechanismTable.java
+++ src/main/java/wsm/graalvm/SemanticMechanismTable.java
@@ -24,8 +24,7 @@ public final class SemanticMechanismTable {
             "1016", SemanticMechanismTable::invoke1016,
             "1022", SemanticMechanismTable::invoke1022,
             "1052", SemanticMechanismTable::invoke1052,
-            "1061", SemanticMechanismTable::invoke1061,
-            "1017", SemanticMechanismTable::invoke1017
+            "1061", SemanticMechanismTable::invoke1061
     );
 
     private SemanticMechanismTable() {}
